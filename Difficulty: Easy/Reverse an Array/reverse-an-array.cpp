22361c43645f2a18c1eecf2ad39017327232911d
SanthoshReddy8 <144278682+SanthoshReddy8@users.noreply.github.com>
2025-08-08 19:27:32 +0530

    Update reverse-an-array.cpp

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -1,3 +1,5 @@
+// Better
+
 class Solution {
   public:
     void reverseArray(vector<int> &arr) {
@@ -7,6 +9,23 @@ class Solution {
             int temp=arr[i];
             arr[i]=arr[n-i-1];
             arr[n-i-1]=temp;
+        }
+    }
+};
+
+
+//Optimised
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int left=0, right=n-1;
+        while(left<right){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
         }
     }
 };
