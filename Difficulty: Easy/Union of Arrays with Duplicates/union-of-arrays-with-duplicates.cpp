0bc8a8fac4a1d36767369ae142a2b3e0aaa90d45
SanthoshReddy8 <144278682+SanthoshReddy8@users.noreply.github.com>
2025-08-12 08:36:38 +0530

    Update union-of-arrays-with-duplicates.cpp

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -1,3 +1,5 @@
+
+//Brute Force
 class Solution {
   public:
     vector<int> findUnion(vector<int>& a, vector<int>& b) {
@@ -18,3 +20,48 @@ class Solution {
         
     }
 };
+
+TC: O(n+m).log(n+m)
+SC: O(1)
+
+//Optimal
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        int m = a.size(), n = b.size();
+        vector<int> unionarr;
+        int i = 0, j = 0;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        while(i < m && j < n) {
+            if(a[i] < b[j]) {
+                if(unionarr.empty() || unionarr.back() != a[i])
+                    unionarr.push_back(a[i]);
+                i++;
+            } else if(b[j] < a[i]) {
+                if(unionarr.empty() || unionarr.back() != b[j])
+                    unionarr.push_back(b[j]);
+                j++;
+            } else {
+                if(unionarr.empty() || unionarr.back() != a[i])
+                    unionarr.push_back(a[i]);
+                i++; j++;
+            }
+        }
+        while(i < m) {
+            if(unionarr.empty() || unionarr.back() != a[i])
+                unionarr.push_back(a[i]);
+            i++;
+        }
+        while(j < n) {
+            if(unionarr.empty() || unionarr.back() != b[j])
+                unionarr.push_back(b[j]);
+            j++;
+        }
+        return unionarr;
+    }
+};
+
+
+TC: O(mlogm + nlogn) for sorting + O(m+n) for merging
+SC: O(1)
